aac53a2ebd3cbca00472773821569a9447550b28
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-11 17:31:38 +0000

    Minor tweak to read filenames from 'extdata' rather than current directory.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nameneedle@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/NameNeedle/inst/util/mkDataset.R
+++ trunk/NameNeedle/inst/util/mkDataset.R
@@ -1,12 +1,19 @@
-sf2Names  <- read.table("sf2Names.tsv",
-                        sep="\t", header=TRUE, row.names=1, as.is=TRUE)
+extdata.dir <- file.path("..", "extdata")
+
+sf2Names.datafile <- file.path(extdata.dir, "sf2Names.tsv")
+sf2Names <- read.delim(sf2Names.datafile, row.names=1, as.is=TRUE)
 sf2Names <- sf2Names$x
-rppaNames <- read.table("rppaNames.tsv",
-                        sep="\t", header=TRUE, row.names=1, as.is=TRUE)
+
+rppaNames.datafile <- file.path(extdata.dir, "rppaNames.tsv")
+rppaNames <- read.delim(rppaNames.datafile, row.names=1, as.is=TRUE)
 rppaNames <- rppaNames$x
-illuNames <- read.table("IlluminaNames.tsv",
-                        sep="\t", header=TRUE, row.names=1, as.is=TRUE)
+
+illuNames.datafile <- file.path(extdata.dir, "IlluminaNames.tsv")
+illuNames <- read.delim(illuNames.datafile, row.names=1, as.is=TRUE)
 illuType <- factor(illuNames$Type)
 illuNames <- illuNames$illuNames
 
-save(sf2Names, rppaNames, illuNames, illuType, file="../../data/cellLineNames.Rda")
+data.dir <- file.path("..", "..", "data")
+dataset <- file.path(data.dir, "cellLineNames.RData")
+save(sf2Names, rppaNames, illuNames, illuType, file=dataset)
+
